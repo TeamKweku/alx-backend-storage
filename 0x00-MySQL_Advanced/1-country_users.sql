0c86293ba52e916ed5cc9a291f80c989376695b7
teamkweku <teamkweku@outlook.com>
2024-01-17 22:27:01 +0000

    checker 7 error fix

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,9 @@
 -- Create a table 'users' with specified attributes and requirements
 -- If the table already exists, the script will not fail
-
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    name VARCHAR(255),
+    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
 );
