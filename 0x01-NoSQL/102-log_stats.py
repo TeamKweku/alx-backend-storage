7e843abfdd6aed76638f55a23c9dd9a7a05d2883
teamkweku <teamkweku@outlook.com>
2024-01-22 13:03:02 +0000

    improved 12-log_stats.py  by adding the top 10 of the most present IPs in the collection nginx of the database logs

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+This script provides statistics about Nginx logs stored in MongoDB
+"""
+
+
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from typing import List
+
+
+def print_log_stats(collection: Collection) -> None:
+    """
+    Display statistics about Nginx logs in MongoDB
+    """
+    methods: List[str] = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    # Total logs
+    total_logs: int = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    # Methods statistics
+    print("Methods:")
+    for method in methods:
+        method_count: int = \
+            collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+
+    # Status check count
+    status_check_count: int = \
+        collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
+
+    # Top 10 IPs
+    top_ips = collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    print("Top 10 IPs:")
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
+
+
+def trigger_logs() -> None:
+    """
+    Function that connects to the MongoDB client and triggers function
+    """
+    client: MongoClient = MongoClient('mongodb://127.0.0.1:27017')
+    log_collection: Collection = client.logs.nginx
+    print_log_stats(log_collection)
+
+
+if __name__ == "__main__":
+    """main function call"""
+    trigger_logs()
